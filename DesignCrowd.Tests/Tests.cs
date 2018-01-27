f1c57feca2813814bbf58e3ff1b79c25fd49d60e
Hesham A. Amin <hspceng@hotmail.com>
2018-01-27 23:40:19 +1100

    initial

---
+++ DesignCrowd.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DesignCrowd.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.False);
+
+        }
+
+    }
+}
---
+++ DesignCrowd/BusinessDayCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignCrowd
+{
+    public class BusinessDayCounter
+    {
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            return 0;
+        }
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
+        {
+            return 0;
+        }
+    }
+}
