180480062d0735469ecc35b48af42e87b68707a5
Hesham A. Amin <hspceng@hotmail.com>
2018-01-28 18:50:04 +1100

    Part 1 - splitting dates

---
+++ DesignCrowd.Tests/BusinessDayCounterTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DesignCrowd.Tests
+{
+    public class BusinessDayCounterTests
+    {
+        [Test]
+        public void WeekdaysBetweenTwoDates_RetrunsDaysWithinGivenRangeExcludingStartAndEnd()
+        {
+            var sut = new BusinessDayCounter();
+            Assert.That(sut.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void WeekdaysBetweenTwoDates_RetrunsOnlyWeekDaysWithinGivenRangeExcludingStartAndEnd()
+        {
+            var sut = new BusinessDayCounter();
+            Assert.That(sut.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)), Is.EqualTo(5));
+        }
+
+        [TestCase(2013, 10, 7, 2014, 1, 1, 61)]
+        [TestCase(2013, 10, 7, 2015, 2, 10, 350)]
+        public void WeekdaysBetweenTwoDatesInDifferentYears_RetrunsOnlyWeekDaysWithinGivenRangeExcludingStartAndEnd(int firstYear, int firstMonth, int firstDay, int secondYear, int secondMonth, int secondDay, int expected)
+        {
+            var sut = new BusinessDayCounter();
+            var firstDate = new DateTime(firstYear, firstMonth, firstDay);
+            var secondDate = new DateTime(secondYear, secondMonth, secondDay);
+            Assert.That(sut.WeekdaysBetweenTwoDates(firstDate, secondDate), Is.EqualTo(expected));
+        }
+
+        [TestCase(2018, 1, 28, 2018, 1, 29)]
+        [TestCase(2018, 1, 29, 2018, 1, 29)]
+        [TestCase(2018, 1, 16, 2018, 1, 22)]
+        public void GetNextMondayTest(int year, int month, int day, int expectedYear, int expectedMonth, int expectedDay)
+        {
+            var actual = BusinessDayCounter.GetNearestMondayDate(new DateTime(year, month, day));
+            var expected = new DateTime(expectedYear, expectedMonth, expectedDay);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
+
+    }
+}
--- DesignCrowd/BusinessDayCounter.cs
+++ DesignCrowd/BusinessDayCounter.cs
@@ -8,8 +8,72 @@ namespace DesignCrowd
     {
         public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            return 0;
+            var adjustedFirstDate = AdjustFirstDate(firstDate);
+            var adjustedSecondDate = AdjustSecondDate(secondDate);
+
+            var nearestMonday = GetNearestMondayDate(adjustedFirstDate);
+
+            if (nearestMonday > adjustedSecondDate)
+            {
+                return (int)adjustedSecondDate.Subtract(adjustedFirstDate).TotalDays;
+            }
+
+            var weekDays = adjustedFirstDate.Equals(nearestMonday) ? 0 : (int)nearestMonday.Subtract(adjustedFirstDate).TotalDays - 2;
+            if (!(firstDate.DayOfWeek == DayOfWeek.Saturday || firstDate.DayOfWeek == DayOfWeek.Sunday))
+            {
+                weekDays--;
+            }
+
+            if (adjustedSecondDate > nearestMonday)
+            {
+                var total = (int)adjustedSecondDate.Subtract(nearestMonday).TotalDays;
+                var weekendDays = 2 * (total / 7);
+                weekDays += total - weekendDays;
+            }
+
+            return weekDays;
         }
+
+        private static DateTime AdjustFirstDate(DateTime firstDate)
+        {
+            var adjustment = 0;
+            switch (firstDate.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                    adjustment = 2;
+                    break;
+                case DayOfWeek.Sunday:
+                    adjustment = 1;
+                    break;
+                default:
+                    adjustment = 0;
+                    break;
+            }
+            return firstDate.AddDays(adjustment);
+        }
+
+        private static DateTime AdjustSecondDate(DateTime secondDate)
+        {
+            var adjustment = 0;
+            switch (secondDate.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    adjustment = -1;
+                    break;
+                default:
+                    adjustment = 0;
+                    break;
+            }
+            return secondDate.AddDays(adjustment);
+        }
+
+        internal static DateTime GetNearestMondayDate(DateTime date)
+        {
+            var days = (8 - ((int)date.DayOfWeek)) % 7;
+            return date.AddDays(days);
+        }
+
+
         public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
         {
             return 0;
